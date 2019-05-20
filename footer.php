f141fb38d8154ed6a4ae4001fd3d6ba693688fae
Lukasz Kalemba <lukasz.kalemba1@gmail.com>
2019-05-20 09:09:06 +0200

    Fill footer.php file with proper content

---
+++ footer.php
@@ -0,0 +1,28 @@
+<footer class="footer">
+  <h2 class="footer__title">Pure Nature</h2>
+
+  <p class="footer__paragraph">Salon Pure Nature powstał w 2012 roku, dzięki pasji dwóch przyjaciółek - Natalii oraz Karoliny, do szeroko pojętej kosmetologii. Obecnie, Salon to 12 niesamowitych osób, które tworzą najlepszy #purenatureteam.</p>
+  <p class="footer__paragraph">Do naszej prac podchodzimy z maksymalnym zaangażowaniem, a kosmetyki oraz sprzęt przez nas wykorzystywany - należy do światowej czołówki. Bądź razem z nami - #purenature !</p>
+
+  <a class="footer__link" href="#">Czytaj dalej...</a>
+
+  <h2 class="footer__title">Szybki kontakt</h2>
+  <p class="footer__paragraph footer__paragraph--lead">Tel. +48 763 332 333</p>
+  <p class="footer__paragraph">Tel. +48 763 332 333</p>
+  <p class="footer__paragraph">Mail: salon@purenature.pl</p>
+
+  <h2 class="footer__title">Bądź na bieżąco</h2>
+  
+  <a class="footer__social-icon" href="#">facebook</a>  
+  <a class="footer__social-icon" href="#">instagram</a>  
+  <a class="footer__social-icon" href="#">youtube</a>  
+  <a class="footer__social-icon" href="#">twitter</a>
+
+  <div class="footer__copyright">
+  <p class="footer__paragraph">&copy; 2019 - Pure Nature</p>
+  <p class="footer__paragraph"><span class="footer__paragraph--lead">Projekt i realizacja: Bracia Konieczni</span></p>
+  </div>  
+
+</footer>
+</body>
+</html>

b09755b3d1d33b3c3487a99421281a2795e95c41
Lukasz Kalemba <lukasz.kalemba1@gmail.com>
2019-05-20 14:49:27 +0200

    Link "montserrat" font to the project

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
   function link_styles_and_scripts() {
     wp_enqueue_style('style', get_theme_file_uri('/dist/css/main.min.css'));
     wp_enqueue_script('js', get_theme_file_uri('/dist/js/index.min.js'), NULL,  microtime(), true);
+    wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Montserrat:400,600,700&display=swap&subset=latin-ext');
   }
 
   add_action('wp_enqueue_scripts', 'link_styles_and_scripts');

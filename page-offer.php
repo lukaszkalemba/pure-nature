a4a83fc9e2d3a300b4921ffe4c1c70ddca176573
Lukasz Kalemba <lukasz.kalemba1@gmail.com>
2019-05-21 13:10:49 +0200

    Link offers on our-offer page

--- page-offer.php
+++ page-offer.php
@@ -8,4 +8,35 @@
 
 <?php get_header(); ?>
 
+
+<section class="section section--offer">
+  <h2 class="section__title">Nasza oferta</h2>
+
+  <?php
+
+  $args = array(
+    'post_type' => 'post',
+    'posts_per_page' => -1
+  );
+
+  $blogposts = new WP_Query($args);
+
+  while($blogposts->have_posts()) {
+    $blogposts->the_post();
+?>
+
+    <div class="post-container">
+
+      <a href="<?php the_permalink(); ?>"><?php echo get_the_post_thumbnail( $page->ID ); ?></a>
+      <h3 class="post__title"><?php the_title(); ?></h3>
+      <a class="post__link" href="<?php the_permalink(); ?>">Dowiedz się więcej</a>
+
+    </div>
+<?php
+  }
+
+?>
+
+</section>
+
 <?php get_footer(); ?>

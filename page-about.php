80be5b5bd8b2f88e254a36515ce5bff0d76cbb83
Lukasz Kalemba <lukasz.kalemba1@gmail.com>
2019-05-21 09:35:53 +0200

    Fix height of carousel and add hero image and content to about page

--- page-about.php
+++ page-about.php
@@ -8,4 +8,16 @@
 
 <?php get_header(); ?>
 
+<img class="about__hero-img" src="<?php bloginfo( 'template_url' );?>/dist/img/offer_hero.png" alt="">
+
+<section class="about-container">
+
+  <?php while ( have_posts() ) : the_post(); ?>
+    <?php the_content();
+  endwhile;
+  wp_reset_query();
+  ?>
+
+</section>
+
 <?php get_footer(); ?>

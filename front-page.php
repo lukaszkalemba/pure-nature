db6b2a82a940c5e72b6b5e237bc5a6a9b1cee739
Lukasz Kalemba <lukasz.kalemba1@gmail.com>
2019-05-21 08:56:53 +0200

    Style carousel at home page

--- front-page.php
+++ front-page.php
@@ -1,46 +1,74 @@
 <?php get_header(); ?>
 
+
 <div class="carousel-container">
-    
-  <div class="carousel slide" data-ride="carousel">
+  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
-      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+      <li class="carousel__indiactior active"
+        data-target="#carouselExampleCaptions"
+        data-slide-to="0"
+      ></li>
+      <li class="carousel__indiactior" data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+      <li class="carousel__indiactior" data-target="#carouselExampleCaptions" data-slide-to="2"></li>
     </ol>
+
+
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="<?php bloginfo( 'template_url' );?>/dist/img/hero.png" class="d-block w-100 carousel__image" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <img
+          src="<?php bloginfo( 'template_url' );?>/dist/img/hero.png"
+          class="carousel__image d-block w-100"
+          alt="..."
+        />
+        <div class="carousel-caption d-md-block">
+          <h5 class="carousel__title">Odkryj swoje prawdziwe piękno</h5>
+          <a role="button" href="<?php the_permalink("280"); ?>" class="carousel__action-link">Dowiedz się więcej</a>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="<?php bloginfo( 'template_url' );?>/dist/img/hero.png" class="d-block w-100 carousel__image" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <img
+          src="<?php bloginfo( 'template_url' );?>/dist/img/hero.png"
+          class="carousel__image d-block w-100"
+          alt="..."
+        />
+        <div class="carousel-caption d-md-block">
+          <h5 class="carousel__title">Odkryj swoje prawdziwe piękno</h5>
+          <a role="button" href="<?php the_permalink("280"); ?>" class="carousel__action-link">Dowiedz się więcej</a>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="<?php bloginfo( 'template_url' );?>/dist/img/hero.png" class="d-block w-100 carousel__image" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Third slide label</h5>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        <img
+          src="<?php bloginfo( 'template_url' );?>/dist/img/hero.png"
+          class="carousel__image d-block w-100"
+          alt="..."
+        />
+        <div class="carousel-caption d-md-block">
+          <h5 class="carousel__title">Odkryj swoje prawdziwe piękno</h5>
+          <a role="button" href="<?php the_permalink("280"); ?>" class="carousel__action-link">Dowiedz się więcej</a>
         </div>
       </div>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <a
+      class="carousel-control-prev"
+      href="#carouselExampleCaptions"
+      role="button"
+      data-slide="prev"
+    >
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
     </a>
-    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <a
+      class="carousel-control-next"
+      href="#carouselExampleCaptions"
+      role="button"
+      data-slide="next"
+    >
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="sr-only">Next</span>
     </a>
   </div>
 </div>
+
 
 
 <section class="offer">

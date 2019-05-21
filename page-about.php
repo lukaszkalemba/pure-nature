db73726203f5d21b74661903a5d7b32d9fba174c
Lukasz Kalemba <lukasz.kalemba1@gmail.com>
2019-05-21 10:18:36 +0200

    Create and style employees section on about page

--- page-about.php
+++ page-about.php
@@ -10,7 +10,7 @@
 
 <img class="about__hero-img" src="<?php bloginfo( 'template_url' );?>/dist/img/offer_hero.png" alt="">
 
-<section class="about-container">
+<section class="about-content">
 
 <h2 class="section__title">Odkrywamy Twoje naturalne piękno</h2>
 
@@ -21,5 +21,33 @@
   ?>
 
 </section>
+
+
+<section class="employees">
+
+<?php
+
+if( have_rows('pracownik') ):
+    while ( have_rows('pracownik') ) : the_row(); ?>
+
+    <article class="employees__employee">
+
+      <?php $image_url = get_sub_field('zdjecie_pracownika')['url']; ?>
+      <img class="employee__img" src="<?php echo $image_url ?>" alt="">
+
+    <h3 class="employee__first-name"><?php the_sub_field('imie_pracownika'); ?></h3>
+    <p class="employee__workplace"><?php the_sub_field('stanowisko_pracownika'); ?></p>
+
+    </article>
+
+<?php
+  endwhile;
+else :
+endif;
+
+?>
+
+</section>
+
 
 <?php get_footer(); ?>

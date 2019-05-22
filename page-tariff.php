2cf96ab6fe97acecb193af209306ccda8634867a
Lukasz Kalemba <lukasz.kalemba1@gmail.com>
2019-05-22 10:19:54 +0200

    Add offers list to the tariff page

--- page-tariff.php
+++ page-tariff.php
@@ -10,6 +10,28 @@
 
   <section class="tariff">
     <h2 class="tariff__title">Cennik</h2>
+
+  <?php
+
+  $args = array(
+    'post_type' => 'post',
+    'posts_per_page' => -1
+  );
+
+  $blogposts = new WP_Query($args);
+
+  while($blogposts->have_posts()) {
+    $blogposts->the_post();
+    ?>
+
+    <p class="tariff__offer-title"><?php the_title(); ?></p>
+
+
+  <?php
+  }
+  ?>
+
+</section>
   </section>
 
 <?php get_footer(); ?>
